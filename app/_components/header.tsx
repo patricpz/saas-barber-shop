ff7b8fec97ccb98fa083b31c151139d888fec089
patricpz <patricbarros19@gmail.com>
2025-07-14 17:56:50 -0300

    feat sidebar menu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,15 +2,23 @@ import { Card, CardContent } from "@/components/ui/card"
 import Image from "next/image"
 import { MenuIcon } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { Sheet, SheetTrigger } from "@/components/ui/sheet"
+import SideBarMenu from "./sidebar-menu"
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-3">
         <Image src="/logo.png" alt="Logo" width={120} height={`8`} />
-        <Button size={"icon"} variant={"outline"}>
-          <MenuIcon />
-        </Button>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size={"icon"} variant={"outline"}>
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SideBarMenu />
+        </Sheet>
       </CardContent>
     </Card>
   )
